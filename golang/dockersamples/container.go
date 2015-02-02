283cacd2c8ef74c95ba0f63e4cb3d61f7895c380
Rajdeep Dua <dua_rajdeep@yahoo.com>
2015-02-01 21:44:39 -0800

    Added unpase and Get state

--- golang/dockersamples/container.go
+++ golang/dockersamples/container.go
@@ -10,74 +10,83 @@ import (
 
 func main() {
     if len(os.Args) >= 2 {
-       arg := os.Args[1]
-       i, err := strconv.Atoi(arg)
-           if err != nil {
+        arg := os.Args[1]
+        i, err := strconv.Atoi(arg)
+        if err != nil {
                // handle error
-               fmt.Println(err)
-               os.Exit(2)
-           }
+            fmt.Println(err)
+            os.Exit(2)
+        }
 
-       if i == 0 {
-           fmt.Printf("Execute getContainers\n")
-           getContainers()
-       } else if i == 1 {
-           fmt.Printf("Execute containerExists\n")
-           if len(os.Args) != 3 {
+        if i == 0 {
+            fmt.Printf("Execute getContainers\n")
+            getContainers()
+        } else if i == 1 {
+            fmt.Printf("Execute containerExists\n")
+            if len(os.Args) != 3 {
                fmt.Println("Please specify container name after 1")
-           } else {
+            } else {
                name := os.Args[2]
                value := containerExists(name)
                fmt.Printf("Container %v exists : %v\n", name, value)
            }
 
-       } else if i == 2 {
-           fmt.Printf("Execute createContainer\n")
-           if len(os.Args) != 3 {
-               fmt.Println("Please specify container name after 2")
-           } else {
-               name := os.Args[2]
-               value := containerExists(name)
+        } else if i == 2 {
+            fmt.Printf("Execute createContainer\n")
+            if len(os.Args) != 3 {
+                fmt.Println("Please specify container name after 2")
+            } else {
+                name := os.Args[2]
+                value := containerExists(name)
 
-               if value  {
-                  fmt.Printf("Container %v exists : %v\n", name, value)
-               } else {
-                  createContainer(name)
-               }
-           }
-       }else if i == 3 {
-           fmt.Printf("Print Container Id\n")
-           if len(os.Args) != 3 {
-               fmt.Println("Please specify container name after 3")
-           } else {
-               name := os.Args[2]
-               printContainerId(name)
-           }
-       }else if i == 4 {
-           fmt.Printf("Start Container Id\n")
-           if len(os.Args) != 3 {
-              fmt.Println("Please specify container name after 4")
-           } else {
-               name := os.Args[2]
-               startContainer(name)
+                if value  {
+                    fmt.Printf("Container %v exists : %v\n", name, value)
+                } else {
+                    createContainer(name)
+                }
             }
-       } else if i == 5 {
-                   fmt.Printf("Show Container process\n")
-                                if len(os.Args) != 3 {
-                                    fmt.Println("Please specify container name after 5")
-                                } else {
-                                    name := os.Args[2]
-                                     listProcesses(name)
-                                }
-       } else if i == 6 {
-                          fmt.Printf("Pause the Container\n")
-                                       if len(os.Args) != 3 {
-                                           fmt.Println("Please specify container name after 6")
+        }else if i == 3 {
+            fmt.Printf("Print Container Id\n")
+            if len(os.Args) != 3 {
+                fmt.Println("Please specify container name after 3")
+            } else {
+                name := os.Args[2]
+                printContainerId(name)
+            }
+        }else if i == 4 {
+            fmt.Printf("Start Container Id\n")
+            if len(os.Args) != 3 {
+                fmt.Println("Please specify container name after 4")
+            } else {
+                name := os.Args[2]
+                startContainer(name)
+            }
+        } else if i == 5 {
+            fmt.Printf("Show Container process\n")
+            if len(os.Args) != 3 {
+                fmt.Println("Please specify container name after 5")
+            } else {
+                name := os.Args[2]
+                listProcesses(name)
+            }
+        } else if i == 6 {
+            fmt.Printf("Pause the Container\n")
+            if len(os.Args) != 3 {
+                fmt.Println("Please specify container name after 6")
                                        } else {
                                            name := os.Args[2]
                                            pauseContainer(name)
                                        }
-       }else {
+        } else if i == 7 {
+            fmt.Printf("Get Container State\n")
+            if len(os.Args) != 3 {
+                fmt.Println("Please specify container name after 6")
+            } else {
+                name := os.Args[2]
+                containerState := getContainerState(name)
+                fmt.Println("Returned State: %#v \n", containerState)
+            }
+       } else {
          fmt.Printf("Please specify valid test no as a command line argument\n")
        }
     } else {
@@ -88,7 +97,8 @@ func main() {
                  "3 [name] : Print Container Id\n" +
                  "4 [name] : StartContainer\n" +
                  "5 [name] : List Processes\n" +
-                 "6 [name] : Pause Container\n"
+                 "6 [name] : Pause Container\n" +
+                 "7 [name] : Get Container State\n"
          fmt.Printf("%v", help)
 
     }
@@ -184,7 +194,6 @@ func getContainerId(name string) (id string) {
            fmt.Printf("name %v\n", name)
            fmt.Printf("name_trimmed %v\n", name_trimmed)
            if name_trimmed == name {
-
                containerId := inspectJSON[i].Id
                fmt.Printf("Container Id : %v", containerId)
                return containerId
@@ -235,3 +244,37 @@ func pauseContainer(name string) {
         }
 }
 
+func unpauseContainer(name string) {
+    containerId := getContainerId(name)
+    if containerId == "" {
+        fmt.Printf("Invalid Container name %v\n", name)
+    } else {
+        body, err := sampleutils.SockRequest("POST", "/containers/" + containerId + "/unpause", nil)
+        if err != nil  {
+            fmt.Printf("\nerror %v\n", err)
+        } else {
+            fmt.Printf("body: %#v\n", body)
+        }
+    }
+}
+func getContainerState(name string) (containerState string) {
+    containerId := ""
+    containerId = getContainerId(name)
+    ///containers/4fa6e0f0c678/json
+    if containerId != "" {
+        body, err := sampleutils.SockRequest("GET", "/containers/" + containerId+ "/json", nil)
+        var inspectJSON struct {
+            Id string
+            State string
+        }
+        if err = json.Unmarshal(body, &inspectJSON); err != nil {
+            fmt.Printf("unable to unmarshal response body: %v", err)
+        }
+        containerState := string(inspectJSON.State)
+
+        return containerState
+    } else {
+        fmt.Printf("Container doesn't exist")
+        return ""
+    }
+}
