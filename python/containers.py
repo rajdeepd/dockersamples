52b00a8c5bb6c9cde29319ad9068add8609201e7
Sriram Patil <spsrirampatil@gmail.com>
2015-02-13 13:54:50 +0530

    Defined a Container class in containers.py. getcontainers returs list of Container objects now. Added a method getcontainerid.

--- python/containers.py
+++ python/containers.py
@@ -1,6 +1,26 @@
 import utils
 import json
 
+class Container:
+	"""
+		self.Command	(str)
+		self.Created	(int)
+		self.Id			(str)
+		self.Image		(str)
+		self.Names		(list)
+		self.Ports		(list)
+		self.Status		(str)
+	"""
+
+	def __init__(self, **entries):
+		self.__dict__.update(entries)
+
+	def __str__(self):
+		s = self.Names[0].strip("/") + "\n"
+		s += self.Id[:10] + "\t" + self.Image + "\n"
+		s += (self.Command + "\t" + self.Status)
+		return s
+
 def getcontainers():
 	handler = utils.RequestHandler()
 	response = handler.request("GET", "/containers/json?all=1", None)
@@ -8,8 +28,23 @@ def getcontainers():
 		print(response)
 		return None
 
-	return response
+	containers_list = json.loads(response)
+	return [Container(**container_dict) for container_dict in containers_list]
+
+def getcontainerid(name):
+	fixed_name = "/" + name
+	containers = getcontainers()
+	for container in containers:
+		if fixed_name in container.Names:
+			return container.Id
+
+	return None
 
 if __name__ == "__main__":
-	jsonresponse = getcontainers()
-	utils.printjson(jsonresponse)
+	#jsonresponse = getcontainers()
+	#utils.printjson(jsonresponse)
+	#for item in jsonresponse:
+	#	print item, "\n"
+	cid = getcontainerid("berserk_colden")
+	if not cid:
+		print "No such container found."
