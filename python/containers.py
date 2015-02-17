7e337aaa7690ab6492801356bce040e11e906613
Rajdeep Dua <dua_rajdeep@yahoo.com>
2015-02-17 00:35:37 -0800

    Changed tabs to spaces

--- python/containers.py
+++ python/containers.py
@@ -1,72 +1,73 @@
 import utils
 import json
 
+#class Containers(object):
 def getcontainers(**params):
-	"""
-		Accepted parameters:
-		all, limit, since, before, size, filters, exited, status
-	"""
-	handler = utils.RequestHandler()
-	url_path = "/containers/json?" + utils.paramstr_from_dict(params)
+    """
+        Accepted parameters:
+        all, limit, since, before, size, filters, exited, status
+    """
+    handler = utils.RequestHandler()
+    url_path = "/containers/json?" + utils.paramstr_from_dict(params)
 
-	response = handler.request("GET", url_path, None)
-	if handler.haserror():
-		print(response)
-		return None
+    response = handler.request("GET", url_path, None)
+    if handler.haserror():
+        print(response)
+        return None
 
-	return json.loads(response)
+    return json.loads(response)
 
 def __searchbyname(name, containers):
-	for container in containers:
-		if name in container.Names:
-			return container.Id
+    for container in containers:
+        if name in container.Names:
+            return container.Id
 
-	return None
+    return None
 
 def __searchbyid(containerid, containers):
-	for container in containers:
-		if fixed_name in container.Names:
-			return container.Names
+    for container in containers:
+        if fixed_name in container.Names:
+            return container.Names
 
-	return None
+    return None
 
 def getcontainerid(name):
-	fixed_name = "/" + name
-	containers = getcontainers()
-	return __searchbyname(fixed_name, containers)
+    fixed_name = "/" + name
+    containers = getcontainers()
+    return __searchbyname(fixed_name, containers)
 
 def containerexists(name = None, containerid = None):
-	containers = getcontainers()
-	if name != None:
-		return (__searchbyname("/" + name, containers) != None)
-	elif containerid != None:
-		return (__searchbyid(containerid, containers) != None)
+    containers = getcontainers()
+    if name != None:
+        return (__searchbyname("/" + name, containers) != None)
+    elif containerid != None:
+        return (__searchbyid(containerid, containers) != None)
 
-	return None
+    return None
 
 def getcontainerinfo(containerid):
-	url = "/containers/" + containerid + "/json"
-	handler = utils.RequestHandler()
-	response = handler.request("GET", url, None)
-	if handler.haserror():
-		print response
-		return None
+    url = "/containers/" + containerid + "/json"
+    handler = utils.RequestHandler()
+    response = handler.request("GET", url, None)
+    if handler.haserror():
+        print response
+        return None
 
-	return json.loads(response)
+    return json.loads(response)
 
 if __name__ == "__main__":
-	containers = getcontainers(all = True)
-	if containers:
-		for item in containers:
-			utils.printjson(json.dumps(item))
+    containers = getcontainers(all = True)
+    if containers:
+        for item in containers:
+            utils.printjson(json.dumps(item))
 
-	cinfo = getcontainerinfo("00731")
-	utils.printjson(obj = cinfo)
+    cinfo = getcontainerinfo("00731")
+    utils.printjson(obj = cinfo)
 
-	"""
-	cid = getcontainerid("berserk_colden")
-	if not cid:
-		print "No such container found."
+    """
+    cid = getcontainerid("berserk_colden")
+    if not cid:
+        print "No such container found."
 
-	print containerexists("berserk_colden")
-	"""
+    print containerexists("berserk_colden")
+    """
--- python/utils.py
+++ python/utils.py
@@ -2,54 +2,56 @@ import httplib
 import socket
 import json
 
+
 class UnixHTTPConnection(httplib.HTTPConnection):
-	"""
-	HTTPConnection object which connects to a unix socket.
-	"""
+    """
+    HTTPConnection object which connects to a unix socket.
+    """
 
-	def __init__(self, sock_path):
-		httplib.HTTPConnection.__init__(self, "localhost")
-		self.sock_path = sock_path
+    def __init__(self, sock_path):
+        httplib.HTTPConnection.__init__(self, "localhost")
+        self.sock_path = sock_path
 
-	def connect(self):
-		self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-		self.sock.connect(self.sock_path)
+    def connect(self):
+        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self.sock.connect(self.sock_path)
+
 
 class RequestHandler:
-	def __init__(self):
-		self.errno = 0
+    def __init__(self):
+        self.err_no = 0
 
-	def request(self, http_method, path, data):
-		self.errno = 0
+    def request(self, http_method, path, data):
+        self.err_no = 0
 
-		try:
-			conn = UnixHTTPConnection("/var/run/docker.sock")
-			conn.connect()
-			conn.request(http_method, path, body = json.dumps(data))
-			response = conn.getresponse()
+        try:
+            conn = UnixHTTPConnection("/var/run/docker.sock")
+            conn.connect()
+            conn.request(http_method, path, body=json.dumps(data))
+            response = conn.getresponse()
 
-			if response.status != 200:
-				self.errno = response.status
+            if response.status != 200:
+                self.err_no = response.status
 
-			return response.read()
-		except Exception as e:
-			self.errno = -1
-			self.msg = str(e)
-			return str(e)
-		finally:
-			conn.close()
+            return response.read()
+        except Exception as e:
+            self.err_no = -1
+            self.msg = str(e)
+            return str(e)
+        finally:
+            conn.close()
 
-	def haserror(self):
-		return (self.errno != 0 and self.errno != 200)
+    def haserror(self):
+        return (self.errno != 0 and self.errno != 200)
 
-def printjson(jsonstr = None, obj = None):
-	if obj == None:
-		obj = json.loads(jsonstr)
-	print(json.dumps(obj, indent = 2, sort_keys = True))
+def printjson(jsonstr=None, obj=None):
+    if obj == None:
+        obj = json.loads(jsonstr)
+    print(json.dumps(obj, indent = 2, sort_keys = True))
 
 def paramstr_from_dict(params):
-	params_str = ""
-	for key in params.keys():
-		params_str += ("&" + key + "=" + str(params[key]))
+    params_str = ""
+    for key in params.keys():
+        params_str += ("&" + key + "=" + str(params[key]))
 
-	return params_str
+    return params_str
